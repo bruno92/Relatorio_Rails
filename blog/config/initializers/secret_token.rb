00daa3b30b0f619491c2a491702a3c0920f43f5e
Bruno Fachi <bruno.fachi@catolicasc.org.br>
2013-09-02 19:48:03 -0400

    Inicio

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '36aa70831307a26af1fac3754c8a81de90fd645a039df02860ec1784dcae7b9ba2e4d70184810172c238f3907563767e229264794aee8f6967d319b4ef69e165'
